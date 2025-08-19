9fcde63789ef0c5125f4eff762ef9afc229e6d1b
Sultan Mahmood Riaz <sultanmr@hotmail.com>
2025-08-19 10:56:23 +0200

    copied into layerd_district-labeling folder

--- labels/scripts/segmentation/__init__.py
+++
@@ -1,8 +0,0 @@
-from .green_spaces import *
-from .crime_statistics import *
-# from .land_prices import *
-# from .playgrounds import *
-# from .regional_statistics import *
-# from .rent_stats import *
-# from .short_time_listings import *
-# from .ubahn import *
